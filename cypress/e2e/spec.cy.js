518ac8413ceef15e99cbaa990ece1bef734db0d3
RomanUkry <121364252+RomanUkry@users.noreply.github.com>
2023-04-16 11:36:17 -0400

    Delete spec.cy.js

--- cypress/e2e/spec.cy.js
+++
@@ -1,7 +0,0 @@
-///<reference types = "Cypress"/>
-describe('empty spec', () => {
-  it('test cypress io example pages', () => {
-    cy.visit('https://example.cypress.io')
-    cy.url().should('eq','https://example.cypress.io/')
-  })
-})
